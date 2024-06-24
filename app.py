ea95bef00e6698370a0b1cda9ff4173922017f77
ccClubProject <ccClubproject@gmail.com>
2024-06-24 15:53:55 +0800

    Update port

--- app.py
+++ app.py
@@ -195,6 +195,6 @@ def handle_location_message(event):
 
 # 當py檔案被直接執行時，__name__變數會是__main__，因此當此條件成立時，代表程式被當作主程式執行，而不是被當作模組引用。
 if __name__ == "__main__":
-    # port = int(os.environ.get('PORT', 5000))
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
 
