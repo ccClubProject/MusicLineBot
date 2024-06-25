9890f9b0d0f895b814c0a1c677d2aca63fad41cd
ccClubProject <ccClubproject@gmail.com>
2024-06-25 10:36:46 +0800

    ADD: drop table if exists

--- backend/init.sql
+++ backend/init.sql
@@ -1,6 +1,11 @@
 -- init.sql schema
 -- 定義資料庫Table Schema(有哪些欄位以及資料型態），一個網站一個table
 
+DROP TABLE IF EXISTS tb_accupass;
+DROP TABLE IF EXISTS tb_kktix;
+DROP TABLE IF EXISTS tb_indievox;
+DROP TABLE IF EXISTS tb_tixcraft;
+
 CREATE TABLE IF NOT EXISTS tb_accupass (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     EventName VARCHAR(255) NOT NULL,
@@ -10,8 +15,8 @@ CREATE TABLE IF NOT EXISTS tb_accupass (
     Artists VARCHAR(255),
     ImageURL VARCHAR(1000),
     PageURL VARCHAR(1000),
-    StartTime DATETIME, -- 活動開始時間，DATETIME時間格式，用來query活動用的
-    EndTime DATETIME -- 活動結束時間，DATETIME時間格式，用來query活動用的
+    StartTime DATETIME, -- DATETIME時間格式，用來query活動用的
+    EndTime DATETIME -- DATETIME時間格式，用來query活動用的
 );
 
 CREATE TABLE IF NOT EXISTS tb_kktix (
