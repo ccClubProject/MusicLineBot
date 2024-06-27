692bb101d7ca3db432272b3c25224c2781270cd7
ccClubProject <ccClubproject@gmail.com>
2024-06-27 16:16:29 +0800

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from search_tracks import *
 import json
 
 # 引入backend資料庫相關自訂模組
-from backend.query_db import *
+# from backend.build import *
 
 app = Flask(__name__)
 channel_access_token = os.environ.get('channel_access_token')
@@ -18,6 +18,9 @@ channel_secret = os.environ.get('channel_secret')
 line_bot_api = LineBotApi(channel_access_token)
 handler = WebhookHandler(channel_secret)
 
+
+# 使用backend模組，將爬蟲資料存進table
+# create_table()
 
 @app.route("/callback", methods=['POST'])
 def callback():
@@ -32,11 +35,9 @@ def callback():
     return 'OK'
 
 
-
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    input_message = event.message.text
-    if input_message.lower() == "live music":
+    if event.message.text.lower() == "live music":
         buttons_template = ButtonsTemplate(
             title='選擇日期',
             text='請選擇',
@@ -57,14 +58,6 @@ def handle_message(event):
             template=buttons_template
         )
         line_bot_api.reply_message(event.reply_token, template_message)
-
-    elif re.match('找', input_message):
-        keyword = input_message.replace("找", "").strip()
-        search_result = search_events(keyword)
-        if len(search_result) != 0:
-            line_bot_api.reply_message(event.reply_token, TextSendMessage(text=search_result))
-        else:
-            line_bot_api.reply_message(event.reply_token, TextSendMessage(text='查無此活動！換個關鍵字吧！'))
 
     else:
         handle_location_message(event)
@@ -251,6 +244,61 @@ else:
     line_bot_api.reply_message(event.reply_token, TextSendMessage(text=message))
 '''
 
+'''
+舊版關鍵字搜尋，都先註解掉
+    #關鍵字搜尋
+    elif re.match('找', message):
+        search = message.replace("找", "").strip()
+        search_word = search.encode("utf-8")
+        search_url_indievox = f"https://www.indievox.com/activity/list/{urllib.parse.quote(search_word)}"
+        search_url_kktix = f"https://kktix.com/events?utf8=%E2%9C%93&search={urllib.parse.quote(search_word)}&start_at=2024%2F06%2F22"
+        search_url_accupass = f"https://www.accupass.com/search?q={urllib.parse.quote(search_word)}"
+        search_url_tixcraft = f"https://tixcraft.com/activity/{urllib.parse.quote(search_word)}"
+        confirm_message = TemplateSendMessage(
+            alt_text='點擊連結前往搜尋結果',
+            template=ButtonsTemplate(
+                title=f"{search}搜尋結果出爐！",
+                text=f"點擊按鈕看{search}有哪些好活動",
+                actions=[
+                    URIAction(
+                        label='馬上前往iNDEIVOX',
+                        uri=search_url_indievox),
+                    URIAction(
+                        label='馬上前往kktix',
+                        uri=search_url_kktix),
+                    URIAction(
+                        label='馬上前往Accupass',
+                        uri=search_url_accupass),
+                    URIAction(
+                        label='馬上前往Tixcraft',
+                        uri=search_url_tixcraft)
+                ]))
+        line_bot_api.reply_message(event.reply_token, confirm_message)
+'''
+
+from flask import Flask, request, abort
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import *
+from search_tracks import *
+
+app = Flask(__name__)
+load_dotenv()
+channel_access_token = os.getenv('channel_access_token')
+channel_secret = os.getenv('channel_secret')
+line_bot_api = LineBotApi(channel_access_token)
+handler = WebhookHandler(channel_secret)
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    signature = request.headers['X-Line-Signature']
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+    return 'OK'
 
 @handler.add(MessageEvent, message=TextMessage)
 def music(event):
@@ -310,25 +358,13 @@ def music(event):
         line_bot_api.reply_message(event.reply_token, carousel_template)
     elif event.message.text in ['流行音樂', '搖滾音樂', '嘻哈音樂', '電子音樂', '爵士音樂', '古典音樂', 'R&B和靈魂音樂', '鄉村音樂']:
         token = get_token()
-        tracks = search_tracks_by_genre(event.message.text, token)
-        if tracks:
+        # 輸出為字典格式
+        track = search_tracks_by_genre(event.message.text, token)
+        if track:
             # 曲目資訊
             columns = []
-            for track in tracks:
-                columns.append(
-                    CarouselColumn(
-                        thumbnail_image_url=track['image_url'],
-                        title=track['title'],
-                        text=track['artist'],
-                        actions=[
-                            URIAction(label='詳細資訊', uri=track['details_url'])
-                        ]
-                    )
-                )
-            carousel_template = TemplateSendMessage(
-                alt_text='選擇曲目',
-                template=CarouselTemplate(columns=columns)
-            )
+            columns.append(CarouselColumn(thumbnail_image_url=track['image_url'], title=track['title'], text=track['artist'], actions=[URIAction(label='詳細資訊', uri=track['details_url'])]))
+            carousel_template = TemplateSendMessage(alt_text='選擇曲目', template=CarouselTemplate(columns=columns))
             line_bot_api.reply_message(event.reply_token, carousel_template)
         else:
             line_bot_api.reply_message(
@@ -337,25 +373,14 @@ def music(event):
             )
     elif event.message.text == '隨機推薦':
         token = get_token()
-        tracks = random_recommendations(token)
-        if tracks:
+        track = random_recommendations(token)
+        if track:
             # 曲目資訊
             columns = []
-            for track in tracks:
-                columns.append(
-                    CarouselColumn(
-                        thumbnail_image_url=track['image_url'],
-                        title=track['title'],
-                        text=track['artist'],
-                        actions=[
-                            URIAction(label='詳細資訊', uri=track['details_url'])
-                        ]
-                    )
-                )
-            carousel_template = TemplateSendMessage(
-                alt_text='選擇曲目',
-                template=CarouselTemplate(columns=columns)
-            )
+            columns.append(
+                CarouselColumn(thumbnail_image_url=track['image_url'], title=track['title'], text=track['artist'],
+                               actions=[URIAction(label='詳細資訊', uri=track['details_url'])]))
+            carousel_template = TemplateSendMessage(alt_text='選擇曲目', template=CarouselTemplate(columns=columns))
             line_bot_api.reply_message(event.reply_token, carousel_template)
     else:
         line_bot_api.reply_message(
@@ -368,8 +393,12 @@ def music(event):
 
 
 
-
 # 當py檔案被直接執行時，__name__變數會是__main__，因此當此條件成立時，代表程式被當作主程式執行，而不是被當作模組引用。
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
     app.run(debug=True, host='0.0.0.0', port=port)
+
+
+
+
+
